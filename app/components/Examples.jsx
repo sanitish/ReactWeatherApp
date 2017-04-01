17cc429eb0a7fa66ebb7fa82754ee89f3816f420
NITISH KUMAR <NITISH KUMAR>
2017-04-01 22:51:03 +0530

    update

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,12 @@ var React = require('react');
 var Examples = React.createClass({
   render: function () {
     return (
+      <div>
       <h3>Examples Component!</h3>
+      <p>Welcome to examples</p>
+      </div>
     )
+
   }
 });
 
